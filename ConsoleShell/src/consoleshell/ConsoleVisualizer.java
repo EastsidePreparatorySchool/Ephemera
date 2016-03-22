4e90557c75665eb4720b42a5c69d4b83b6136db4
Gunnar Mein <gmein@eastsideprep.org>
2016-03-22 14:45:03 -0700

    Meaningful output for actual games in the ConsoleShell

--- ConsoleShell/src/consoleshell/ConsoleVisualizer.java
+++ ConsoleShell/src/consoleshell/ConsoleVisualizer.java
@@ -15,19 +15,43 @@ import java.util.Scanner;
 public class ConsoleVisualizer implements GameVisualizer {
     @Override
     public void showCompletedTurn() {
+        System.out.println("Turn complete.");
     }
+    
+    @Override
+    public void showMove(String packageName, String className, int newX, int newY, int energy, int tech) {
+        System.out.print(packageName + ":" + className + " moved to (");
+        System.out.print(Integer.toString(newX) + "," + Integer.toString(newY));
+        System.out.println ("), E:" + Integer.toString(energy) + ", T:" + Integer.toString(tech));
+    }
+
 
     @Override
     public void showFightBefore(int x, int y, String[] participants, int[] fightPowers) {
-
+        System.out.println();
+        System.out.println("Them aliens is fightin' again:");
+        for (int i=0; i < participants.length; i++) {
+            System.out.print("Alien \"" + participants[i] + "\" ");
+            System.out.println ("is fighting with energy " + Integer.toString(fightPowers[i]));
+        }
+        System.out.println();
     }
 
     @Override
     public void showFightAfter(int x, int y, String[] participants, int[] newEnergy, int[] newTech) {
+        System.out.println();
+        System.out.println("Here is what is left from that fight:");
+        for (int i=0; i < participants.length; i++) {
+            System.out.print("Alien \"" + participants[i] + "\" ");
+            System.out.print(" now has energy " + Integer.toString(newEnergy[i]));
+            System.out.print(" and new tech level " + Integer.toString(newTech[i]));
+        }
+        System.out.println();
     }
 
     @Override
     public void showSection(int x, int y, int width) {
+        System.out.println("Showing sections costs extra");
     }
 
     @Override
