ae528e5e66fb7bc0bc3f11383653d7480501bed5
Gunnar Mein <gmein@eastsideprep.org>
2016-04-28 21:05:34 -0700

    fix to deployment batch file

--- deploy_to_spacecritters.cmd
+++ deploy_to_spacecritters.cmd
@@ -2,6 +2,7 @@ md ..\spacecritters
 md ..\spacecritters\lib
 md ..\spacecritters\aliens
 md ..\spacecritters\aliens\examples
+md ..\spacecritters\alieninterfaces
 
 
 copy spacecritters\dist\spacecritters.jar ..\spacecritters
@@ -9,4 +10,5 @@ copy spacecritters\dist\lib\alieninterfaces.jar ..\spacecritters\lib
 copy spacecritters\dist\lib\gameengine.jar ..\spacecritters\lib
 copy spacecritters\dist\lib\stockaliens.jar ..\spacecritters\lib
 copy customalien\dist\customalien.jar ..\spacecritters\aliens\examples
+copy alieninterfaces\src\alieninterfaces\* ..\spacecritters\alieninterfaces
 copy sc_config.csv ..\spacecritters
