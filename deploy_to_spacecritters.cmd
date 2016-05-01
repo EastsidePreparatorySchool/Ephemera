f509d5aad978b841f06f5354669c50fb42c5a8b1
Gunnar Mein <gmein@eastsideprep.org>
2016-04-30 17:53:55 -0700

    deploy batch changes

--- deploy_to_spacecritters.cmd
+++ deploy_to_spacecritters.cmd
@@ -1,16 +1,17 @@
 md ..\spacecritters
 md ..\spacecritters\lib
+rd /s ..\spacecritters\aliens
 md ..\spacecritters\aliens
 md ..\spacecritters\examples
-md ..\spacecritters\examples\customalien
 md ..\spacecritters\alieninterfaces
-
+rd /s ..\spacecritters\logs
 
 copy spacecritters\dist\spacecritters.jar ..\spacecritters
 copy spacecritters\dist\lib\alieninterfaces.jar ..\spacecritters\lib
 copy spacecritters\dist\lib\alieninterfaces.jar ..\spacecritters\alieninterfaces
 copy spacecritters\dist\lib\gameengine.jar ..\spacecritters\lib
 copy spacecritters\dist\lib\stockaliens.jar ..\spacecritters\lib
-xcopy customalien\* ..\spacecritters\examples\customalien /s /y
+copy customalien\src\customalien\customalien.java ..\spacecritters\examples\
+copy stockaliens\src\stockaliens\aggressivealien.java ..\spacecritters\examples\
 copy alieninterfaces\src\alieninterfaces\* ..\spacecritters\alieninterfaces
 copy sc_config.csv ..\spacecritters
