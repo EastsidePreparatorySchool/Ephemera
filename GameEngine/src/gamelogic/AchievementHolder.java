7f80d971d48d5b945c98818a6ba95f577bba1507
guberti <gavin.uberti@gmail.com>
2016-05-10 15:35:28 -0700

    Begin Implementing Achievements

---
+++ GameEngine/src/gamelogic/AchievementHolder.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gamelogic;
+import java.util.HashMap;
+import gameengineinterfaces.*;
+
+/**
+ *
+ * @author guberti
+ */
+public class AchievementHolder {
+    HashMap<String, int[]> map;
+    
+    AchievementHolder() {
+        
+    }
+    void RecieveFlag(AchievementFlag flag) {
+        
+    }
+    void CheckAchievementsForAlien(AlienContainer ac) {
+        
+    }
+}
---
+++ GameEngine/src/gamelogic/AchievementReq.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gamelogic;
+
+import gameengineinterfaces.*;
+
+/**
+ *
+ * @author guberti
+ */
+public class AchievementReq {
+    int energyReq;
+    int techReq;
+    int tradeReq;
+    int fightingEnergyReq;
+    int aliensCountReq;
+    int broadcastReq;
+    AchievementFlag flagReq;
+}
