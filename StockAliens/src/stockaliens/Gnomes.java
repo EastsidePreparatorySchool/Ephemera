6c1784b190f48c4547ea2744ece528d2e5e76408
TheProfessor2642 <ditto2642@gmail.com>
2018-01-25 19:33:39 -0800

    added gnomes class

---
+++ StockAliens/src/stockaliens/Gnomes.java
@@ -0,0 +1,51 @@
+/*
+ * This work is licensed under a Creative Commons Attribution-NonCommercial 3.0 United States License.
+ * For more information go to http://creativecommons.org/licenses/by-nc/3.0/us/
+ */
+package stockaliens;
+
+import alieninterfaces.*;
+
+/**
+ *
+ * @author ditto2642
+ */
+
+public class Gnomes implements Alien{
+	Context ctx;
+	final boolean debug = true;
+	
+	public void init(Context ctx, int id, int parent, String message) {
+
+
+	}
+
+	
+	public Direction getMove() {
+
+		return null;
+	}
+
+	
+	public Action getAction() {
+
+		return null;
+	}
+
+
+	@Override
+	public void communicate() {
+		//throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	}
+
+	@Override
+	public void receive(String[] messages) {
+		//throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	}
+
+	@Override
+	public void processResults() {
+		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	}
+
+}
