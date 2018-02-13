71471bfc62b963e86ec8c8729bbef9fe86a63b1e
TheProfessor2642 <ditto2642@gmail.com>
2018-02-13 10:59:45 -0800

    added example get to server.js

--- Web/server.js
+++ Web/server.js
@@ -3,6 +3,13 @@ app = express();
 app.use(express.static(__dirname+'/public'));
 const port = 3000;
 
+//example app.get:
+app.get('/example', (req, res)=>{
+  // you can access things like cookies with the req object, use res.send() or res.sendFile() to send a response
+  res.send("<!DOCTYPE html><html><body>this is an example</body></html>");
+  //thats if you want /example to be an accessible page, you could also send plaintext or a json, which could be used by the client
+});
+
 app.listen(port, (err) => {
   if (err) return console.log('error', err);
 
