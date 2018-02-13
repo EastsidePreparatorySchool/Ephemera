53569ab08fd30dd1714c023cf294e647c3664f58
TheProfessor2642 <ditto2642@gmail.com>
2018-02-13 10:48:14 -0800

    added grid, changed formatting, removed useless key handlers

--- Web/public/script.js
+++ Web/public/script.js
@@ -1,13 +1,24 @@
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
-var cubeGeo = new THREE.BoxGeometry( 1, 1, 1 );
-camera.position.z = 5;
-camera.position.y = 5;
-//camera.rotation.x = -Math.PI/4;
+
+var cubeGeo = new THREE.BoxGeometry(1, 1, 1);
+
+var size = 501;
+var divisions = 501;
+
+var gridHelper = new THREE.GridHelper( size, divisions );
+scene.add( gridHelper );
+
+camera.position.z = 100;
+camera.position.y = 100;
+camera.rotation.x = -Math.PI/4;
+
+var light = new THREE.AmbientLight(0x404040);
+scene.add(light);
+
 document.addEventListener('keyup', keyUp, false);
 document.addEventListener('keydown', keyDown, false);
 function keyUp(event) {
@@ -20,52 +31,9 @@ function keyDown(event) {
 var rotation = 0;
 var key = {
 check: () => {
-  if(key[65]) { //a
-    cubeMesh.position.x -= 0.05;
-    //console.log("left");
-  }
-
-  if(key[87]) { //w
-    cubeMesh.position.z -= 0.05;
-    //console.log("up");
-  }
-
-  if(key[68]) { //d
-    cubeMesh.position.x += 0.05;
-    //console.log("right");
-  }
-
-  if(key[83]) { //s
-    cubeMesh.position.z += 0.05;
-    //console.log("down");
-  }
-  if(key[69]){
-    cubeMesh.position.y += 0.05;
-  }
-  if(key[81]){
-    cubeMesh.position.y -= 0.05;
-  }
-  if(key[37]){
-    rotation += 0.7;
-    if(rotation>360){
-      rotation = 0;
-    }
-  }
-  if(key[39]){
-    rotation -= 0.7;
-    if(rotation<0){
-      rotation = 360;
-    }
-  }
+  //put things that happen while keys are pushed here
 }
 }
-//the following is unnesasary, as it was previously used for camera rotating, but wasn't workings
-camera.position.z = 5*Math.cos((rotation*Math.PI)/180);
-camera.position.x = 5*Math.sin((rotation*Math.PI)/180);
-camera.rotation.x = 0;
-camera.rotation.y = (rotation*Math.PI)/180;
-camera.rotation.x = -Math.PI/4;
-
 
 
 const alien  = class{
@@ -75,6 +43,7 @@ const alien  = class{
     scene.add(this.mesh);
     this.mesh.position.x = x;
     this.mesh.position.z = z;
+    this.mesh.position.y = 0.5;
   }
   move(x,z){
     this.mesh.position.x += x;
@@ -106,6 +75,6 @@ function tick(){
 /*  camera.rotation.x = (rotation*Math.PI)/180;
   camera.rotation.z = -(rotation*Math.PI)/180;*/
   //console.log('tick');
-  renderer.render( scene, camera );
+  renderer.render(scene,camera);
 }
 var interval = setInterval(tick,30);
