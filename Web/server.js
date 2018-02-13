800f12ab41f1a7f03c6376ca07ca8c2a6ae42d7c
TheProfessor2642 <ditto2642@gmail.com>
2018-02-13 10:52:33 -0800

    Made server.js actually work as a server

--- Web/server.js
+++ Web/server.js
@@ -1,3 +1,10 @@
 const express = require('express');
 app = express();
 app.use(express.static(__dirname+'/public'));
+const port = 3000;
+
+app.listen(port, (err) => {
+  if (err) return console.log('error', err);
+
+  console.log('listening on port ' + port);
+});
